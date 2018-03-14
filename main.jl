841e05b03b380ed7f7fbb5ae40eacd8d54d0f6bd
Gilga <gilgatx@gmail.com>
2018-03-14 09:36:27 +0100

    *

--- main.jl
+++ main.jl
@@ -13,7 +13,7 @@ versioninfo()
 
 # OS X-specific GLFW hints to initialize the correct version of OpenGL
 GLFW.Init()
-		
+    
 # Create a windowed mode window and its OpenGL context
 window = GLFW.CreateWindow(WIDTH, HEIGHT, "OpenGL Example")
 # Make the window's context current
@@ -81,7 +81,7 @@ setProjection(CAMERA, projection_perspective(60.0f0, RATIO, 0.001f0, 10000.0f0))
 Update(CAMERA)
 
 #MVP *= transform([0.0f0,0.0f0,0.0f0],[0.0f0,0.0f0,0.0f0],Float32[])
-#mvp *= translation([0,0,-1.0f0])		
+#mvp *= translation([0,0,-1.0f0])    
 location_mvp = glGetUniformLocation(program, "mvp")
 
 setMVP(mvp) = glUniformMatrix4fv(location_mvp, 1, false, mvp)
@@ -109,13 +109,13 @@ const STARTY = -(COUNTY*dist) / 2.0f0
 const STARTZ = -(COUNTZ*dist*2) / 2.0f0
 
 type INDEX
-	x::Integer
-	y::Integer
-	z::Integer
-	pos::SMatrix{4,4,Float32}
-	mvp::SMatrix{4,4,Float32}
-	
-	INDEX() = new(0,0,0,zeros(SMatrix{4,4,Float32}),zeros(SMatrix{4,4,Float32}))
+  x::Integer
+  y::Integer
+  z::Integer
+  pos::SMatrix{4,4,Float32}
+  mvp::SMatrix{4,4,Float32}
+  
+  INDEX() = new(0,0,0,zeros(SMatrix{4,4,Float32}),zeros(SMatrix{4,4,Float32}))
 end
 
 blocks = INDEX[]; refblocks = Ptr{Void}[]
@@ -124,67 +124,67 @@ for i=1:COUNT push!(blocks,INDEX()); push!(refblocks, pointer_from_objref(blocks
 x=0; y=0; z=0; w=0;
 
 for b in blocks
- x += 1
- if x >= COUNTX
-  y += 1; x=0;
-  if y >= COUNTY z += 1; y=0;
-   if z >= COUNTX w+=1; z=0;
-    if w > 1 error("invalid range"); end
-   end
+  x += 1
+  if x >= COUNTX
+    y += 1; x=0;
+    if y >= COUNTY z += 1; y=0;
+      if z >= COUNTX w+=1; z=0;
+        if w > 1 error("invalid range"); end
+      end
+    end
   end
- end
- 
- b.x = STARTX+dist*x
- b.y = STARTY+dist*z
- b.z = STARTZ-dist*y
 
- b.pos = SMatrix{4,4,Float32}(translation([b.x,b.y,b.z]))
+  b.x = STARTX+dist*x
+  b.y = STARTY+dist*z
+  b.z = STARTZ-dist*y
+
+  b.pos = SMatrix{4,4,Float32}(translation([b.x,b.y,b.z]))
 end
 
 function updateBlocks()
-	mmvp = SMatrix{4,4,Float32}(CAMERA.MVP)
-	for b in blocks; b.mvp=mmvp*b.pos; end
+  mmvp = SMatrix{4,4,Float32}(CAMERA.MVP)
+  for b in blocks; b.mvp=mmvp*b.pos; end
 end
 
 const ic = icount #optimized
 
 # Loop until the user closes the window
 render = function(x)
- #mvp = mmvp*MMatrix{4,4,Float32}(unsafe_wrap(Array, mvp, (4,4)))
- #setMVP(CAMERA.MVP*translation([c.x,c.y,c.z]))
- 
- glUniformMatrix4fv(location_mvp, 1, false, x.mvp)
- glDrawElements(GL_TRIANGLES, ic, GL_UNSIGNED_INT, C_NULL )
- #glDrawArrays(GL_TRIANGLES, 0, vcount)
- nothing
+  #mvp = mmvp*MMatrix{4,4,Float32}(unsafe_wrap(Array, mvp, (4,4)))
+  #setMVP(CAMERA.MVP*translation([c.x,c.y,c.z]))
+
+  glUniformMatrix4fv(location_mvp, 1, false, x.mvp)
+  glDrawElements(GL_TRIANGLES, ic, GL_UNSIGNED_INT, C_NULL )
+  #glDrawArrays(GL_TRIANGLES, 0, vcount)
+  nothing
 end
 
 if compileAndLink
-	objptr = createLoop(1,refblocks,render) #compileAndLink
-	const loopBlocks() = loopByObject(objptr) #compileAndLink
+  objptr = createLoop(1,refblocks,render) #compileAndLink
+  const loopBlocks() = loopByObject(objptr) #compileAndLink
 else
-	const bb = blocks
-	const loopBlocks() = for b in bb; render(b); end
+  const bb = blocks
+  const loopBlocks() = for b in bb; render(b); end
 end
 
 i=0
 while !GLFW.WindowShouldClose(window)
- showFrames()
- #UpdateCounters()
- if OnUpdate(CAMERA) updateBlocks() end
+  showFrames()
+  #UpdateCounters()
+  if OnUpdate(CAMERA) updateBlocks() end
 
- # Pulse the background
- c=0.5 * (1 + sin(i * 0.01)); i+=1
- glClearColor(c, c, c, 1.0)
- glClear(GL_COLOR_BUFFER_BIT)
+  # Pulse the background
+  c=0.5 * (1 + sin(i * 0.01)); i+=1
+  glClearColor(c, c, c, 1.0)
+  glClear(GL_COLOR_BUFFER_BIT)
 
- #print("loopBlocks "); @time
- loopBlocks()
+  #print("loopBlocks "); @time
+  loopBlocks()
    
- # Swap front and back buffers
- GLFW.SwapBuffers(window)
- # Poll for and process events
- GLFW.PollEvents()
+  # Swap front and back buffers
+  GLFW.SwapBuffers(window)
+  # Poll for and process events
+  GLFW.PollEvents()
 end
-	
+  
 GLFW.Terminate()
