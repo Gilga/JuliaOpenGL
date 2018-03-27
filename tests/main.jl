df995dcc18baa6eaaf7e042fb87f29dac3225f9e
Gilga <gilgatx@gmail.com>
2018-03-27 22:37:36 +0200

    added 0.6.2 support

--- tests/main.jl
+++ tests/main.jl
@@ -1,3 +1,17 @@
-function main()
+__precompile__()
+
+module App
+
+using JLD
+using Images
+using ImageMagick
+
+function run()
   include("run.jl")
+end
+
+end
+
+function main()
+  App.run()
 end
--- tests/run.jl
+++ tests/run.jl
@@ -1,4 +1,26 @@
 println("run...")
 
+imgfile="../blocks.png"
+imgarrayfile="$imgfile.jld"
+
 using Images
-Images.load("C:/Users/Mario/Desktop/3DSimulations/JuliaOpenGL/blocks.png")
+img = Images.load(imgfile)
+(imgwidth, imgheight) = size(img)
+
+#img = Gray.(img) #grayImage
+#imgMatrix = reinterpret.(channelview(img)) 
+#println("write ",typeof(imgMatrix),"->" , size(imgMatrix))
+
+#imga = reinterpret.(vec(channelview(rgbImage)))
+
+#open(f -> print(f, imga), imgarrayfile, "w+")
+#writecsv(imgarrayfile, imgMatrix)
+#a = readcsv(imgarrayfile)
+#println("read ",typeof(a),"->" , size(a))
+#a = convert(Array{UInt8},a)
+#println("convert ",typeof(a), "->", a[1:100],"\n")
+
+using JLD
+#JLD.save(imgarrayfile, "img", imgMatrix)
+a = JLD.load(imgarrayfile, "img")
+println("read ",typeof(a),"->" , size(a))
