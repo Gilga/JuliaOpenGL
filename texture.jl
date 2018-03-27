d72ca3e3e52db9078d13e81d2183294925aad4f7
Gilga <gilgatx@gmail.com>
2018-03-27 21:08:58 +0200

    added raw image

--- texture.jl
+++ texture.jl
@@ -1,8 +1,12 @@
 function uploadTexture(path)
-  img = Images.load(path)
-  (imgwidth, imgheight) = size(img)
-  imga = reinterpret.(vec(channelview(img)))
-
+  #img = Images.load(path)
+  #(imgwidth, imgheight) = size(img)
+  #imga = reinterpret.(vec(channelview(img)))
+  
+  # alternative loading raw image
+  (imgwidth, imgheight) = (512,512)
+  imga = convert(Array{UInt8},readcsv("$path.csv"))
+  
   texture = glGenTexture()
   glActiveTexture(GL_TEXTURE0)
   glBindTexture(GL_TEXTURE_2D, texture)
@@ -10,5 +14,5 @@ function uploadTexture(path)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) #GL_LINEAR
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE) #GL_CLAMP_TO_EDGE,GL_REPEAT
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE) #GL_CLAMP_TO_EDGE,GL_REPEAT
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, imgwidth, imgheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, pointer(imga))
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, imgwidth, imgheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imga)
 end
