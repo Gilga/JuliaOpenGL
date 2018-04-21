550aa8c4a917d7ebe8b613f4a95de5e32b46dc89
Gilga <gilgatx@gmail.com>
2018-04-21 11:02:08 +0200

    fixed path

--- build-0.6.1.bat
+++ build-0.6.1.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/Users/%username%/AppData/Local/Julia-0.6.1/bin/julia.exe" "src/build.jl" "0.6.1" "JuliaOpenGL" "build" "src/JuliaOpenGL.jl"
+"C:/Users/%username%/AppData/Local/Julia-0.6.1/bin/julia.exe" "src/build.jl" "0.6.1" "JuliaOpenGL" "build" "JuliaOpenGL.jl"
 pause
