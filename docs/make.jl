0d3ec886dc1c90e30e1f6da761494bb8ae5633d1
Gilga <gilgatx@gmail.com>
2018-04-21 11:20:34 +0200

    setting up deployment

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,10 @@ push!(LOAD_PATH,"../src/")
 using Documenter
 
 makedocs()
+
+deploydocs(
+  deps   = Deps.pip("mkdocs", "python-markdown-math"),
+  repo = "https://github.com/Gilga/JuliaOpenGL",
+  julia  = "0.6.2",
+  osname = "osx"
+)
