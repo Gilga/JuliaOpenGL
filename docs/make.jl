e15ea2fdfb8f788e54a6ddcebf3cffcf5d1ebe2c
Gilga <gilgatx@gmail.com>
2018-04-21 12:37:22 +0200

    *

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,9 @@ makedocs()
 deploydocs(
   deps   = Deps.pip("mkdocs", "python-markdown-math"),
   repo = "https://github.com/Gilga/JuliaOpenGL",
+  branch = "gh-pages",
+  latest = "master",
   julia  = "0.6.2",
-  osname = "osx"
+  root = "docs/build",
+  target = "site",
 )
